cc3cd55ece8ba06b561074efb1fc280a480fcac9
noth1ngreal <dominichx001@gmail.com>
2026-08-05 05:43:05 -0700

    dll injection

---
+++ DLL_injection/msgboxDLL.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+
+#pragma comment (lib, "user32.lib")
+
+BOOL WINAPI DllMain(HINSTANCE hModule, DWORD reason, LPVOID lpvReserved) {
+    switch (reason) {
+        case DLL_PROCESS_ATTACH:
+            MessageBoxW(NULL, L"Ferrari 812 GTS", L"Malwareeeeee", MB_ICONQUESTION | MB_OK);
+            break;
+    }
+    return true;
+}
